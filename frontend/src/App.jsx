237ec3242f9e98b3dee0bd83521e03c45970e689
ofcdigisphere-labs <ofcdigisphere@gmail.com>
2026-07-23 20:40:11 +0700

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,27 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import TelegramPage from './pages/TelegramPage';
+// Halaman baru yang ditambahkan
+import GamesPage from './pages/GamesPage';
+import PulsaPage from './pages/PulsaPage';
+import InternetPage from './pages/InternetPage';
 
 function App() {
   return (
     <Router>
       <Toaster position="top-right" />
       <Routes>
+        {/* Rute Utama & Layanan Umum */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/topup" element={<TopupPage />} />
         <Route path="/telegram" element={<TelegramPage />} />
+        
+        {/* Rute Layanan Baru */}
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/pulsa" element={<PulsaPage />} />
+        <Route path="/internet" element={<InternetPage />} />
+
+        {/* Rute Transaksi & Autentikasi */}
         <Route path="/payment/:orderId" element={<PaymentPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
