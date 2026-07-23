a6a868b76fd622d25b512055496e332ad3311ae6
ofcdigisphere-labs <ofcdigisphere@gmail.com>
2026-07-23 20:43:08 +0700

    PulsaPage.jsx

--- frontend/src/pages/PulsaPage.jsx
+++ frontend/src/pages/PulsaPage.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+function GamesPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Halaman Games</h2>
+    </div>
+  );
+}
+
+export default GamesPage;
 
